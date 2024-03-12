47ffafef717cb8143659f878e7f1751c72c20306
1stChaS <chananthida.so@ku.th>
2024-03-12 16:11:35 +0700

    first_draft

--- counter/test_counter.py
+++ counter/test_counter.py
@@ -7,3 +7,43 @@
 
    You can use pytest or unittest.
 """
+import unittest
+import pytest
+from circle import Circle
+
+
+# Circle.add_area
+class Test_Area(unittest.TestCase):
+    """Testcase for add.area"""
+
+    # question 1
+    def test_radius_positive(self, other):
+        self.circle = Circle
+        if Circle.add_area() > 0 and other.radius > 0:
+            self.assertTrue(self.circle)
+            self.assertTrue(other.radius)
+        raise ValueError("The radius should always be positive number.")
+
+    # question 2
+    def test_one_zero(self, other):
+        if self.assertEqual(0, self.circle):
+            self.assertNotEqual(0, self.circle)
+        elif self.assertEqual(0, other.circle):
+            self.assertNotEqual(0, other.circle)
+
+    # question 3
+    def raise_negative(self):
+        with self.assertRaises(ValueError):
+            self.circle.Circle()
+
+        # # pytest
+        # with pytest.raises(ValueError):
+        #     self.radius.Circle()
+
+
+
+
+
+
+
+
