f82aaffb4af0b819447e303ac08660fe3693b4fa
1stChaS <chananthida.so@ku.th>
2024-03-13 17:37:25 +0700

    correct singleton

---
+++ counter/counter.py
@@ -0,0 +1,43 @@
+"""A singleton counter.
+   counter = Counter()  get a reference to the counter. Initial count is 0.
+   counter.count        property returns the current count
+   counter.increment()  add 1 to current count and also return the new value
+   Requirements:
+   1. in Counter, do not use any static methods except __new__.
+      You may not have a __new__ depending on how you implement the singleton.
+"""
+
+
+class Counter:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.__count = 1
+        return cls._instance
+
+    @property
+    def count(self):
+        return self.__count
+
+    def increment(self):
+        self.__count += 1
+        return self.__count
+
+    def __del__(self):
+        del self.__count
+
+    def __str__(self):
+        return f"{self.__count}"
+
+
+if __name__ == "__main__":
+    s1 = Counter()
+    s2 = Counter()
+
+    # Test Singleton
+    if id(s1) == id(s2):
+        print("Singleton works, both variables contain the same instance.")
+    else:
+        print("Singleton failed, variables contain different instances.")
--- counter/test_counter.py
+++ counter/test_counter.py
@@ -1,16 +1,27 @@
 """Write a unit test to verify that Counter is a singleton.
 
    Also verify that all instances share the same count
-   and that the count is not reset to 0 when you invoke 
-   count = Counter() 
+   and that the count is not reset to 0 when you invoke
+   count = Counter()
    after the first time.
 
    You can use pytest or unittest.
 """
+import unittest
+from counter import Counter
 
 
+class TestCounter(unittest.TestCase):
+    def test_counter(self):
+        """Testcase from Readme"""
+        counter_1 = Counter()
+        self.assertEqual(counter_1.count, 1)
+        self.assertEqual(counter_1.count, 1)  # invoking count doesn't change anything
+        self.assertEqual(counter_1.increment(), 2)  # add 1 and return the new count
 
-
-
-
-
+        counter_2 = Counter()
+        self.assertTrue(counter_2 is counter_1)
+        self.assertEqual(counter_2.count, 2)  # shares the same count
+        self.assertEqual(counter_2.increment(), 3)  # add 1 and return the new count
+        self.assertEqual(counter_1.count, 3)
+        self.assertEqual(counter_1.count, 3)  # counter_1 doesn't set to zero
